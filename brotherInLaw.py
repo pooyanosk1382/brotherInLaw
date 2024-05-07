aa2acb785f58b69834d3b65b120f84abcecd301e
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-07 18:20:35 +0330

    finished the main task

--- brotherInLaw.py
+++ brotherInLaw.py
@@ -1,47 +1,40 @@
 import threading
 import time
 
-# Define a barrier with 2 threads
 barrier = threading.Barrier(2)
 
 
 def firstBrotherInLaw():
     print("First family start the trip")
+    time.sleep(1)
     for _ in range(4):
-        # Thread 1 waits for 2 seconds
         print("First family start going to next toll")
         time.sleep(2)
 
-        # Thread 1 reaches the barrier
         print("First family reached the toll " + str(_+1))
         barrier.wait()
 
-        # Thread 1 continues execution
         print("First family meet the second family")
 
 
 def secondBrotherInLaw():
     print("second family start the trip")
+    time.sleep(1)
     for _ in range(4):
-        # Thread 2 waits for 4 seconds
         print("Second family start going to next toll")
         time.sleep(4)
 
-        # Thread 2 reaches the barrier
         print("Second family reached the toll " + str(_+1))
         barrier.wait()
 
-        # Thread 2 continues execution
         print("Second family meet the first family")
 
 
-# Create and start the threads
 firstGroom = threading.Thread(target=firstBrotherInLaw)
 secondGroom = threading.Thread(target=secondBrotherInLaw)
 firstGroom.start()
 secondGroom.start()
 
-# Wait for the threads to finish
 firstGroom.join()
 secondGroom.join()
 
