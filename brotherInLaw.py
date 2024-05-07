2bfe8d9ed560228de12106f97488be08e8250918
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-07 15:22:21 +0330

    finished the main task

--- brotherInLaw.py
+++ brotherInLaw.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import threading
+import time
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Define a barrier with 2 threads
+barrier = threading.Barrier(2)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def firstBrotherInLaw():
+    for _ in range(4):
+        # Thread 1 waits for 2 seconds
+        print("First family start going to next toll")
+        time.sleep(2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        # Thread 1 reaches the barrier
+        print("First family reached the toll " + str(_+1))
+        barrier.wait()
+
+        # Thread 1 continues execution
+        print("They meet the second family")
+
+
+def secondBrotherInLaw():
+    for _ in range(4):
+        # Thread 2 waits for 4 seconds
+        print("Second family start going to next toll")
+        time.sleep(4)
+
+        # Thread 2 reaches the barrier
+        print("Second family reached the toll " + str(_+1))
+        barrier.wait()
+
+        # Thread 2 continues execution
+        print("They meet the first family")
+
+
+# Create and start the threads
+firstGroom = threading.Thread(target=firstBrotherInLaw)
+secondGroom = threading.Thread(target=secondBrotherInLaw)
+firstGroom.start()
+secondGroom.start()
+
+# Wait for the threads to finish
+firstGroom.join()
+secondGroom.join()
+
+print("All threads have completed.")
